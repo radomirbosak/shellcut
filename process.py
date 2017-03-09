5583c7ac5b1c69399234700e37982dfaa2cd3b8f
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-09 15:21:42 +0100

    Put python code to functions

--- process.py
+++ process.py
@@ -12,19 +12,37 @@ HOME = os.environ['HOME']
 configdir = os.path.join(HOME, '.config', 'shellcut.d')
 
 
-shortcuts = []
-for filename in glob.glob(os.path.join(configdir, '*.yaml')):
-    y = yaml.load(open(filename))
-    shortcuts.extend(y['shortcuts'])
+def load_shortcuts():
+    """
+    Load shortcuts from the ~/.config/shellcut.d/directory
+    """
+    shortcuts = []
+    for filename in glob.glob(os.path.join(configdir, '*.yaml')):
+        y = yaml.load(open(filename))
+        shortcuts.extend(y['shortcuts'])
+    return shortcuts
 
-u = sys.argv[1]
 
-for cut in shortcuts:
-    result = parse.parse(cut['match'], u)
-    if result is not None:
-        print(cut['shell'].format(*result.fixed, **result.named))
-        break
-else:
-    sys.exit(1)
+def check_shortcuts(input_data, shortcuts):
+    """
+    Check for each every shortcut if the input string matches it
+    """
+    for cut in shortcuts:
+        result = parse.parse(cut['match'], input_data)
+        if result is not None:
+            print(cut['shell'].format(*result.fixed, **result.named))
+            break
+    else:
+        sys.exit(1)
 
-sys.exit(0)
+    sys.exit(0)
+
+
+def main():
+    u = sys.argv[1]
+    shortcuts = load_shortcuts()
+    check_shortcuts(u, shortcuts)
+
+
+if __name__ == '__main__':
+    main()
