b8b9ad0ad61bd69e5c6e1ea55bd3de1893785932
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-17 21:46:20 +0100

    Add option to label patterns
    This commit adds the possiblity to specify an optional label to
    distinguish between multiple matching patterns.
    
    Before this, the first matching pattern was always used.
    
    Fixes #10

--- scripts/shellcut.py
+++ scripts/shellcut.py
@@ -24,31 +24,60 @@ def load_shortcuts():
     return shortcuts
 
 
-def check_shortcuts(input_data, shortcuts):
+def check_shortcuts(input_data, shortcuts, label=None):
     """
     Check for each every shortcut if the input string matches it
-    """
-    for cut in shortcuts:
-        if 'match' in cut:
-            result = parse.parse(cut['match'], input_data)
-            if result is not None:
-                print(cut['shell'].format(*result.fixed, **result.named))
-                break
-        if 'regex' in cut:
-            match = re.match(cut['regex'], input_data)
-            if match:
-                print(cut['shell'].format(*match.groups()))
-                break
-    else:
-        sys.exit(1)
 
-    sys.exit(0)
+    Returns: replaced string
+    """
+    possible = []
+
+    for cut in shortcuts:
+        match = get_match(input_data, cut, label)
+        if match is not None:
+            possible.append(match)
+
+    if not possible:
+        return None
+
+    return possible[0]
+
+
+def get_match(input_data, shortcut, label=None):
+    """
+    Check if 'input_data' matches the 'shortcut' pattern and if yes, return the
+    substituted shell command.
+    """
+    if label and shortcut.get('label') != label:
+        return None
+
+    if 'match' in shortcut:
+        result = parse.parse(shortcut['match'], input_data)
+        if result is not None:
+            return shortcut['shell'].format(*result.fixed, **result.named)
+
+    if 'regex' in shortcut:
+        match = re.match(shortcut['regex'], input_data)
+        if match:
+            return shortcut['shell'].format(*match.groups())
+    return None
 
 
 def main():
+    # load CLI arguments
     u = sys.argv[1]
+    label = sys.argv[2] if len(sys.argv) >= 3 else None
+
+    # load and check shortcuts
     shortcuts = load_shortcuts()
-    check_shortcuts(u, shortcuts)
+    command_string = check_shortcuts(u, shortcuts, label=label)
+
+    # if the function returned None, we have no match
+    if command_string is None:
+        sys.exit(1)
+
+    # print the string, which is later interpreted by bash/fish
+    print(command_string)
 
 
 if __name__ == '__main__':
