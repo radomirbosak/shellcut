4d2c3f57ddbc6371495558717c458f21eaabe2b5
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-18 09:50:12 +0100

    Add unit test for check_shortcuts

--- tests/test_shellcut.py
+++ tests/test_shellcut.py
@@ -1,8 +1,20 @@
-from unittest import TestCase, skip
+from unittest import TestCase
+from unittest.mock import patch
+
+import shellcut
 
 
 class TestShellcut(TestCase):
 
-    @skip('Not implemented')
-    def test_main(self):
-        pass
+    @patch('shellcut.get_match')
+    def test_check_shortcuts(self, mock_get_match):
+        """
+        Test that check_shortcuts returns the shell string for the first
+        matching shortcut
+        """
+        mock_get_match.side_effect = [None, 'shell2', 'shell3']
+
+        shortcuts = ['shortcut1', 'shortcut2', 'shortcut3']
+        result = shellcut.check_shortcuts('input_data', shortcuts, label=None)
+
+        self.assertEquals(result, 'shell2')
