03e147b54a8381fe012e06dce6dd0b2a9e38c79c
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-29 08:21:18 +0200

    Add more information to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
     name='shellcut',
     version='0.2.0',
     packages=['shellcut'],
+    description='Shell shortcuts',
+    author='Radomír Bosák',
+    author_email='radomir.bosak@gmail.com',
+    url='https://github.com/radomirbosak/shellcut',
+    download_url='https://github.com/radomirbosak/shellcut/archive/0.2.0.tar.gz',
+    keywords=['shell', 'shortcuts', 'regex'],
     package_data={'': ['config/default.yaml']},
     entry_points={
         'console_scripts': [
