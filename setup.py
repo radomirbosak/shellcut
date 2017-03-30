577d562efa9abd0459baa62ce8e3a8cace8908ea
Radomír Bosák <radomir.bosak@gmail.com>
2017-03-30 16:21:24 +0200

    Satisfy pep8 in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     author='Radomír Bosák',
     author_email='radomir.bosak@gmail.com',
     url='https://github.com/radomirbosak/shellcut',
-    download_url='https://github.com/radomirbosak/shellcut/archive/0.2.1.tar.gz',
+    download_url='https://github.com/radomirbosak/shellcut/archive/'
+                 '0.2.1.tar.gz',
     keywords=['shell', 'shortcuts', 'regex'],
     package_data={'': ['config/default.yaml']},
     entry_points={
